02f5e94a009834f8da599594f1324cbb3a3e640b
Soenke Sothmann <soenke.sothmann@gmail.com>
2014-01-31 09:12:15 +0100

    Fixed encoding (UTF-8)

--- examples/JavaTypescriptWebapp/src/main/ts/demo.ts
+++ examples/JavaTypescriptWebapp/src/main/ts/demo.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014 Sönke Sothmann
+ * Copyright (C) 2014 SÃ¶nke Sothmann
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
